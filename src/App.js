08c49d79e5453215f77081d3e4d7dec2b031a14d
ateshun <emmanuel.ackah@azubiafrica.org>
2023-09-05 13:38:48 +0000

    FInal Work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from "react";
 import "./App.css";
 import NewPage from "./component/NewPage";
+import sedan from "./images/icon-sedans.svg";
+import suvs from "./images/icon-suvs.svg";
+import luxury from "./images/icon-luxury.svg";
 
 function App() {
   return (
     <div className="App">
-      <NewPage />
+      <div className="SEDAN">
+        <NewPage
+          icons={sedan}
+          title="SEDAN"
+          text="Choose a sedan for its affordability and excellent fuel economy.
+            Ideal for cruising in the city or on your next road trip."
+          btn="Learn More"
+        />
+      </div>
+      <div className="SUV">
+        <NewPage
+          icons={suvs}
+          title="SUVS"
+          text="Take an SUV for its spacious interior, power, and versatility.Perfect for your next family vacation and off-road adventures."
+          btn="Learn More"
+        />
+      </div>
+      <div className="LUXURY">
+        <NewPage
+          icons={luxury}
+          title="LUXURY"
+          text="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
+          btn="Learn More"
+        />
+      </div>
     </div>
   );
 }
--- src/component/NewPage.js
+++ src/component/NewPage.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-function NewPage() {
-  return <div>NewPage</div>;
+function NewPage(props) {
+  return (
+    <div className="container">
+      <img src={props.icons} alt="icon" />
+      <h2>{props.title}</h2>
+      <p>{props.text}</p>
+      <button className="btnn">{props.btn}</button>
+    </div>
+  );
 }
 
 export default NewPage;
